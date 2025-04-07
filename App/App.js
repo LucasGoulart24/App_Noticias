f306ccc02a50b53865393a91bc67e650a3477e78
LucasGoulart24 <Goulart7720@gmail.com>
2025-04-07 20:45:35 -0300

    noticia

--- App/App.js
+++ App/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 
 export default function App() {
-  let Imagem = 'https://s2-g1.glbimg.com/8FnTAN34UJ4AXEoF74NOOeLXBSQ=/0x0:1920x1080/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2025/4/z/PadxMjSHGqWQmNSS0Kdw/rj1-limpo-20250407-1115-frame-184036.jpeg'
+  let Imagem = 'https://s2-g1.glbimg.com/8FnTAN34UJ4AXEoF74NOOeLXBSQ=/0x0:1920x1080/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2025/4/z/PadxMjSHGqWQmNSS0Kdw/rj1-limpo-20250407-1115-frame-184036.jpeg';
+  let Imagem2 = 'https://s2-g1.glbimg.com/7j3ZbbnWbNrCEP04cr8-c8fMhBg=/0x0:1267x714/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2025/k/C/jTnzAHSoW8xx4m4XoFKw/thumbnail-image.png';
+  let Imagem3 = 'https://s2-g1.glbimg.com/d6_4nnb-aGdUGPtZaBG29Fy2b5o=/0x0:640x360/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2022/b/7/BV2rRZTTehZnmBFfZxag/image006-6-.jpg';
   return (
     <ScrollView style={styles.container}>
       <View style={styles.banner}>
@@ -14,12 +16,12 @@ export default function App() {
           <Text style={styles.texto}>Lady Gaga no Rio: palco começa a ser montado na praia de Copacabana</Text>
         </View>
         <View style={styles.card}>
-          <Image source={{uri: Imagem}} style={{ width: '100%', height: 240 }}></Image>
-          <Text style={styles.texto}>Lady Gaga no Rio: palco começa a ser montado na praia de Copacabana</Text>
+          <Image source={{uri: Imagem2}} style={{ width: '100%', height: 240 }}></Image>
+          <Text style={styles.texto}>Acidente no Morro dos Cavalos: BR-101 é liberada após caminhão tombar, explodir e incendiar veículos em SC</Text>
         </View>
         <View style={styles.card}>
-          <Image source={{uri: Imagem}} style={{ width: '100%', height: 240 }}></Image>
-          <Text style={styles.texto}>Lady Gaga no Rio: palco começa a ser montado na praia de Copacabana</Text>
+          <Image source={{uri: Imagem3}} style={{ width: '100%', height: 240 }}></Image>
+          <Text style={styles.texto}>Curiosity encontra moléculas relacionadas à vida em Marte</Text>
         </View>
       </View>
     </ScrollView>
