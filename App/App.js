4c5b96b346c2c81b87e7d752d14d1833a0b11bd7
LucasGoulart24 <Goulart7720@gmail.com>
2025-04-08 16:05:35 -0300

    noticia

--- App/App.js
+++ App/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity } from 'react-native';
 
 export default function App() {
   let Imagem = 'https://s2-g1.glbimg.com/8FnTAN34UJ4AXEoF74NOOeLXBSQ=/0x0:1920x1080/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2025/4/z/PadxMjSHGqWQmNSS0Kdw/rj1-limpo-20250407-1115-frame-184036.jpeg';
@@ -14,14 +14,23 @@ export default function App() {
         <View style={styles.card}>
           <Image source={{uri: Imagem}} style={{ width: '100%', height: 240 }}></Image>
           <Text style={styles.texto}>Lady Gaga no Rio: palco começa a ser montado na praia de Copacabana</Text>
+          <TouchableOpacity style={styles.botao}>
+            <Text style={{ color: "#FFF", fontSize: 14 }}>Saber Mais</Text>
+          </TouchableOpacity>
         </View>
         <View style={styles.card}>
           <Image source={{uri: Imagem2}} style={{ width: '100%', height: 240 }}></Image>
           <Text style={styles.texto}>Acidente no Morro dos Cavalos: BR-101 é liberada após caminhão tombar, explodir e incendiar veículos em SC</Text>
+          <TouchableOpacity style={styles.botao}>
+            <Text style={{ color: "#FFF", fontSize: 14 }}>Saber Mais</Text>
+          </TouchableOpacity>
         </View>
         <View style={styles.card}>
           <Image source={{uri: Imagem3}} style={{ width: '100%', height: 240 }}></Image>
           <Text style={styles.texto}>Curiosity encontra moléculas relacionadas à vida em Marte</Text>
+          <TouchableOpacity style={styles.botao}>
+            <Text style={{ color: "#FFF", fontSize: 14 }}>Saber Mais</Text>
+          </TouchableOpacity>
         </View>
       </View>
     </ScrollView>
@@ -50,14 +59,23 @@ const styles = StyleSheet.create({
   },
   card: {
     width: 380,
-    borderRadius: 10,
-    backgroundColor: '#292929',
+    borderRadius: 5,
+    backgroundColor: '#D9D9D9',
     margin: 5
   },
   texto: {
-    color: "#FFF",
+    color: "#000",
     fontSize: 17,
     fontWeight: 600,
-    margin: 8
+    margin: 5
+  },
+  botao: {
+    width: 100,
+    height: 40,
+    borderRadius: 5,
+    backgroundColor: "#292929",
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 5
   }
 });
